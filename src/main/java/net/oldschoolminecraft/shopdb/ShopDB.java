cec5aaab8c5cec7f068fe6b36572a3374688c282
moderatorman <12193288+moderatorman@users.noreply.github.com>
2025-02-24 03:14:16 -0800

    Add command to run indexing manually

--- src/main/java/net/oldschoolminecraft/shopdb/ShopDB.java
+++ src/main/java/net/oldschoolminecraft/shopdb/ShopDB.java
@@ -3,7 +3,11 @@ package net.oldschoolminecraft.shopdb;
 import com.Acrobot.ChestShop.ChestShop;
 import com.google.gson.Gson;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
@@ -27,30 +31,55 @@ public class ShopDB extends JavaPlugin
         if (chestShop == null)
             System.out.println("[ShopDB] ChestShop appears to not have been loaded yet. The first sweep will be delayed by 3 minutes.");
 
-        getServer().getScheduler().scheduleAsyncRepeatingTask(this, () ->
-        {
-            Instant start = Instant.now();
-            ArrayList<SearchRegion> searchRegions = config.getSearchRegions();
-            for (SearchRegion region : searchRegions)
-            {
-                World world = Bukkit.getWorld(region.worldName);
-                List<WrappedShop> shops = ShopUtils.getShopsInRegion(world, region.startX, region.endX, region.startZ, region.endZ);
-                List<ShopDataModel> serializable = new ArrayList<>();
-                for (WrappedShop shop : shops)
-                    serializable.add(shop.getSerializable());
-                try (FileWriter writer = new FileWriter(config.getString("dataExportFile")))
-                {
-                    gson.toJson(serializable, writer);
-                } catch (IOException e) {
-                    System.err.println("Failed to save shop data!");
-                    e.printStackTrace(System.err);
-                }
-            }
-            Instant duration = Instant.now().minus(start.getNano(), ChronoUnit.NANOS);
-            System.out.println("[ShopDB] Finished shop DB update in " + TimeUnit.NANOSECONDS.toSeconds(duration.getNano()) + " seconds");
-        }, (chestShop == null) ? (20 * 60) * 3 : 0L, (20 * 60) * 60); // once per hour with a 3-minute initial delay from startup if chest shop hasn't been loaded yet
+        // run shop index once per hour with a 3-minute initial delay from startup if chest shop hasn't been loaded yet
+        getServer().getScheduler().scheduleAsyncRepeatingTask(this, () -> runShopIndex(null), (chestShop == null) ? (20 * 60) * 3 : 0L, (20 * 60) * 60);
 
         System.out.println("ShopDB enabled");
+    }
+
+    private synchronized void runShopIndex(CommandSender sender)
+    {
+        Instant start = Instant.now();
+        ArrayList<SearchRegion> searchRegions = config.getSearchRegions();
+        for (SearchRegion region : searchRegions)
+        {
+            World world = Bukkit.getWorld(region.worldName);
+            List<WrappedShop> shops = ShopUtils.getShopsInRegion(world, region.startX, region.endX, region.startZ, region.endZ);
+            if (sender != null) sender.sendMessage(ChatColor.GRAY + "Found " + shops.size() + " shop(s) in region: " + region.regionName);
+            List<ShopDataModel> serializable = new ArrayList<>();
+            for (WrappedShop shop : shops)
+                serializable.add(shop.getSerializable());
+            try (FileWriter writer = new FileWriter(config.getString("dataExportFile")))
+            {
+                gson.toJson(serializable, writer);
+            } catch (IOException e) {
+                System.err.println("Failed to save shop data!");
+                e.printStackTrace(System.err);
+            }
+        }
+        Instant duration = Instant.now().minus(start.getNano(), ChronoUnit.NANOS);
+        String msg = "Finished shop DB update in " + TimeUnit.NANOSECONDS.toSeconds(duration.getNano()) + " seconds";
+        System.out.println("[ShopDB] " + msg);
+        if (sender != null) sender.sendMessage(ChatColor.GREEN + msg);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+    {
+        if (label.equalsIgnoreCase("runshopindex"))
+        {
+            if (!(sender.hasPermission("shopdb.runindex") || sender.isOp()))
+            {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to run this command!");
+                return true;
+            }
+
+            sender.sendMessage(ChatColor.GRAY + "Running shop index...");
+            runShopIndex(sender);
+            return true;
+        }
+
+        return false;
     }
 
     public void onDisable()
